4f56e760b945dae074d321cf1ee4e189cd1fcf6b
Welliton Klein <welliton.fokushima@gmail.com>
2020-01-06 13:13:52 -0200

    Adicionando uma entrada de localizaçao e o pacote 'location'

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -3,8 +3,9 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../widgets/image_input.dart';
 import '../providers/great_places.dart';
+import '../widgets/image_input.dart';
+import '../widgets/location_input.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = '/add-place';
@@ -49,10 +50,10 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                       decoration: InputDecoration(labelText: 'Title'),
                       controller: _titleController,
                     ),
-                    SizedBox(
-                      height: 10,
-                    ),
+                    const SizedBox(height: 10),
                     ImageInput(_selectImage),
+                    const SizedBox(height: 10),
+                    LocationInput(),
                   ],
                 ),
               ),
