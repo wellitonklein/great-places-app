dad4ee2f1348c6293ea8dfb4a119c7e7ce01b404
Welliton Klein <welliton.fokushima@gmail.com>
2020-01-06 15:23:07 -0200

    Permitindo que os usuarios escolham um local no mapa

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -19,11 +19,28 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng _pickedLocation;
+
+  void _selectLocation(LatLng position) {
+    setState(() {
+      _pickedLocation = position;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Map'),
+        actions: <Widget>[
+          if (widget.isSelecting)
+            IconButton(
+              icon: const Icon(Icons.check),
+              onPressed: (_pickedLocation == null)
+                  ? null
+                  : () => Navigator.of(context).pop(_pickedLocation),
+            )
+        ],
       ),
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
@@ -33,6 +50,15 @@ class _MapScreenState extends State<MapScreen> {
           ),
           zoom: 16,
         ),
+        onTap: (widget.isSelecting) ? _selectLocation : null,
+        markers: (_pickedLocation == null)
+            ? null
+            : {
+                Marker(
+                  markerId: MarkerId('m1'),
+                  position: _pickedLocation,
+                ),
+              },
       ),
     );
   }
