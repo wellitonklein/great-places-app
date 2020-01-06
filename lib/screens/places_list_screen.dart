c09e0c8b7053933e415ddca3f7769c71383bcff6
Welliton Klein <welliton.fokushima@gmail.com>
2020-01-06 16:48:11 -0200

    Adicionando uma tela 'Placedetail' e abrindo o mapa no modo 'Somente leitura'

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import './add_place_screen.dart';
 import '../providers/great_places.dart';
+import './add_place_screen.dart';
+import './place_detail_screen.dart';
 
 class PlacesListScreen extends StatelessWidget {
   @override
@@ -44,7 +45,10 @@ class PlacesListScreen extends StatelessWidget {
                           title: Text(greatPlaces.items[i].title),
                           subtitle: Text(greatPlaces.items[i].location.address),
                           onTap: () {
-                            // Go to detail page ...
+                            Navigator.of(context).pushNamed(
+                              PlaceDetailScreen.routeName,
+                              arguments: greatPlaces.items[i].id,
+                            );
                           },
                         ),
                       ),
